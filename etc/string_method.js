4e34bbb49d8fc3aae473df531a8fe8ca4cf9fdf8
cholee <cholee@yedam.ac>
2022-04-24 19:42:57 +0900

    'ah'

--- etc/string_method.js
+++ etc/string_method.js
@@ -1,6 +1,6 @@
 // string_method.js
 
-const str1  = '1 + 2 * 3';
+const str1 = '1 + 2 * 3';
 const str2 = new String('1 + 2 * 3');
 
 console.log(eval(str1));
@@ -8,3 +8,6 @@ console.log(eval(str2.valueOf()));
 
 const str3 = ' 문자열 입력 공백 제거 테스트 ';
 console.log(str3.trimStart().trimEnd());
+
+let result = ' 문자열 입력    공백 제거 테스트 '.split(' ').filter(el => el != '') .join(' ');
+console.log(result);
