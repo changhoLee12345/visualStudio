9f585853e66c4792de439c384f7bef6ed0739f31
cholee <cholee@yedam.ac>
2022-04-28 11:24:16 +0900

    'a'

---
+++ etc/dom/dom.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    
+    let htmlNode = document.documentElement;
+    console.log(htmlNode);
+    let bodyNode = htmlNode.lastChild;
+    console.log(htmlNode.lastChild);
+})
