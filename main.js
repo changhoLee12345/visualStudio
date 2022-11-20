6d1b021efe20168ac888c2993cd54d378a64bf5a
newleadon <newleadon@email.com>
2022-11-20 23:00:09 +0900

    module

--- main.js
+++ main.js
@@ -8,4 +8,14 @@
 // } = 'family';
 // console.log(ryon);
 
-console.log(null);
+import modfnc from './module.js';
+modfnc.setData([{
+    name: 'hong',
+    age: 20
+}, {
+    name: 'kim',
+    age: 22
+}])
+console.log(modfnc.makeTable())
+document.getElementById('show').append(modfnc.makeTable());
+// console.log(modfnc);
