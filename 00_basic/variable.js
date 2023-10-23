344e5393b33387d595dcfe219d77b5a2bebb42a2
you <you@email.co>
2023-10-23 10:52:00 +0900

    end

---
+++ 00_basic/variable.js
@@ -0,0 +1,16 @@
+// variable.js
+
+let {
+    ryon,
+    apeach,
+    con
+} = 'family';
+let arrNames = ['ryon', 'apeach', 'con']
+// let {
+//     ryon,
+//     apeach,
+//     con
+// } = arrNames;
+
+
+console.log(ryon)
